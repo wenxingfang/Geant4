98be2e496f0208810871a23e5c539ed5b69cf147
wenxingfang <1473717798@qq.com>
2018-03-10 06:28:25 +0100

    Update Note.sh

--- Note.sh
+++ Note.sh
@@ -1,4 +1,4 @@
-Ubantu 16.04
+#Ubantu 16.04
 
 GEANT4_USE_QT (DEFAULT : OFF)
   apt-file search Qt5CoreConfig.cmake
@@ -9,11 +9,13 @@ GEANT4_USE_RAYTRACER_X11 (DEFAULT : OFF, Unix only)
 GEANT4_USE_XM (DEFAULT : OFF, Unix Only)
   sudo apt-get install libmotif-dev
 
-Bulid Applications
+#Bulid Applications
 cd $HOME/B1-build
 cmake -DGeant4_DIR=/home/you/geant4-install/lib/Geant4-G4VERSION $HOME/B1
+make
 
-Problem:
+
+#Problem:
 Q:The System Running In Low Graphics Mode
 A:http://simpledeveloper.com/system-running-in-low-graphics-mode/
   https://askubuntu.com/questions/141606/how-to-fix-the-system-is-running-in-low-graphics-mode-error
